8c617d9965cdd810d21fe75f553e03ff2e94c6c6
rosdelac <rodrigosierradlc@gmail.com>
2016-09-27 20:21:43 -0500

    Update PublicoVsPrivado.rb

--- PublicoVsPrivado.rb
+++ PublicoVsPrivado.rb
@@ -1,35 +1,33 @@
 class Employee
-  attr_reader :name,:email
-  attr_writer :salary,:deposit_account
-
-
-  def initialize (name, email, salary, deposit_account)
+  attr_reader :deposit_account, :name
+  attr_writer :salary
+  def initialize(name,email,salary,deposit_account)
     @name = name
     @email = email
     @salary = salary
     @deposit_account = deposit_account
-    @deposit_account_censored = @deposit_account.gsub(/^(\d{3})-(\d{2})(\d{1})-(\d{3})/, '*****\3\4')
   end
-
+  private
   def coefficient
     coefficients = { 1 => 0...1_000, 1.2 => 1_000...2_000, 1.4 => 2_000...5_000, 1.5 => 5_000..10_000 }
 
     coefficients.find { |coefficient, range| range.include? @salary }.first
   end
-
-  def to_s
-    "#{@name} <email: #{@email}> acct: #{@deposit_account_censored}"
+  def censoraccount(account)
+    account.gsub(/^(\d{3})-(\d{2})(\d{1})-(\d{3})/, '*****\3\4')
   end
-
+  public
   def vacation_days
     coefficient * 7  
   end
-
   def bonus
     coefficient * 1000
   end
+  def to_s
+    "#{@name} <email: #{@email}> acct: #{censoraccount(@deposit_account)}"
+  end
+  
 end
-
 employee = Employee.new('Juan Perez', 'juan@gmail.com', 1_800, '123-456-512')
 
 employee.to_s
